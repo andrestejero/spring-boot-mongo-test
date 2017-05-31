a9c5d35d08bb84d7f73429622d19cab773de125c
Andres Tejero <atejero@garbarino.com.ar>
2017-05-31 11:25:23 -0300

    fix

--- src/main/java/com/andrestejero/TeteMongo/models/User.java
+++ src/main/java/com/andrestejero/TeteMongo/models/User.java
@@ -1,26 +1,20 @@
 package com.andrestejero.TeteMongo.models;
 
-import com.sun.istack.internal.Nullable;
-
 public class User {
 
-    @Nullable
     private final String firstName;
 
-    @Nullable
     private final String lastName;
 
-    public User(@Nullable String firstName, @Nullable String lastName) {
+    public User(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    @Nullable
     public String getFirstName() {
         return firstName;
     }
 
-    @Nullable
     public String getLastName() {
         return lastName;
     }
